688de74fa7f7e857edb44540ff1c8909a0917307
jefflunt <jefflunt@gmail.com>
2016-12-21 08:50:12 -0600

    Ref #10 - Moved to github issue

--- mini_pubsub.rb
+++ mini_pubsub.rb
@@ -3,7 +3,6 @@ require_relative './errors/end_of_stdin'
 require_relative './channel_serializer'
 require_relative './receivers/stdin_receiver'
 
-# TODO: wrap serialization with begin/rescue
 router = Router.new(ChannelSerializer.read_file)
 receiver = STDINReceiver.new
 puts "Router: #{router.inspect}"
@@ -16,7 +15,6 @@ rescue MiniPubSub::EndOfSTDIN, Interrupt => e
   print "Normal shutdown due to #{e.class.name}. Exiting ... "
 end
 
-# TODO: wrap serialization with begin/rescue
 ChannelSerializer.write_file
 
 puts 'DONE'
