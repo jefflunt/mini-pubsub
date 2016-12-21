c48c02727275328362460366e644edddbb73c84b
jefflunt <jefflunt@gmail.com>
2016-12-21 08:52:04 -0600

    Ref #1 - Moved to github issue

--- receivers/stdin_receiver.rb
+++ receivers/stdin_receiver.rb
@@ -2,9 +2,6 @@ require_relative '../errors/end_of_stdin'
 require_relative '../message'
 
 class STDINReceiver
-  # TODO: All receivers need to implement message sanitization, because the
-  # receivers are the border component that receives raw input from the outside
-  # world.
   def initialize
     puts 'Receiving on STDIN ...'
   end
