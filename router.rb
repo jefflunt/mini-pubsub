9695c8c9ceadcfa82afcc1fe6ebbbd346500ba25
jefflunt <jefflunt@gmail.com>
2016-12-30 21:20:15 -0600

    Ref #57 - Indent router output differently

--- router.rb
+++ router.rb
@@ -41,10 +41,10 @@ class Router
 
     @channels.each do |channel_name, channel|
       str << "  ##{channel_name}:\n"
-      str << "  logger: #{channel.logger.to_s}\n"
-      str << "  subscribers:\n"
+      str << "    logger: #{channel.logger.to_s}\n"
+      str << "    subscribers:\n"
       channel.subscribers.each do |subscriber|
-      str << "    #{subscriber.id}: #{subscriber.sender}\n"
+      str << "      #{subscriber.id}: #{subscriber.sender}\n"
       end
     end
 
