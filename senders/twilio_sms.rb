c4648f063845e5b9ce28d6f11eabbf12798d83d5
jefflunt <jefflunt@gmail.com>
2017-01-25 21:00:11 -0600

    Ref #70 - Spaces for tabs fix.

--- senders/twilio_sms.rb
+++ senders/twilio_sms.rb
@@ -11,13 +11,13 @@ class TwilioSMS < NoneSender
 
   def publish(msg)
     puts "Sending: #{@account_sid} #{@auth_token} #{@from} #{@to} #{msg}"
-		client = Twilio::REST::Client.new @account_sid, @auth_token
+    client = Twilio::REST::Client.new @account_sid, @auth_token
 
 		client.account.messages.create({
-			from: @from,
-			to:   @to,
+      from: @from,
+      to:   @to,
       body: msg.body
-		})
+    })
   end
 
   def to_s
