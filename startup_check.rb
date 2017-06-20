e80e9cd3e303593cfda42875194b2c6798a8a35d
Jeff Lunt <jefflunt@gmail.com>
2017-06-20 13:15:50 -0500

    Ref #72 - Less code.

--- startup_check.rb
+++ startup_check.rb
@@ -1,10 +1,6 @@
 module StartupCheck
   def self.run
-    check_args
-  end
-
-  def self.check_args
-    if ARGV.length != 1
+    unless ARGV.length == 1
       puts "Usage:"
       puts "  mini-pubsub.rb <path to config file>"
       exit 1
