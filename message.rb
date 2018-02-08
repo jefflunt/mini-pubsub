c20187944936aa05d20a227090bfa1d4f67d5f8e
Jeff Lunt <jefflunt@gmail.com>
2018-02-08 13:25:16 -0600

    Simpler message splitting

--- message.rb
+++ message.rb
@@ -2,10 +2,7 @@ class Message
   attr_reader :command, :channel, :body
 
   def initialize(sanitized_input_str)
-    parts = sanitized_input_str.split(' ')
-    @command = parts[0]
-    @channel = parts[1][1..-1]
-    @body = parts[2..-1].join(' ')
+    @command, @channel, @body = sanitized_input_str.split(' ', 3)
   end
 
   def to_s
